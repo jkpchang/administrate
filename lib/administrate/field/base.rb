111caa7d343276576d20514e2f8de8e57a4603c9
Jorge Chang <jorge@jorgechang.com>
2016-10-11 16:43:29 -0700

    add "Upload" form to "has_many" field if upload link is provided.

--- lib/administrate/field/base.rb
+++ lib/administrate/field/base.rb
@@ -40,6 +40,10 @@ module Administrate
         options.fetch(:readonly, false)
       end
 
+      def image?
+        options.fetch(:image, false)
+      end
+
       def reorder_action
         options.fetch(:reorder_action, false)
       end
--- lib/administrate/version.rb
+++ lib/administrate/version.rb
@@ -1,3 +1,3 @@
 module Administrate
-  VERSION = "0.2.9".freeze
+  VERSION = "0.2.10".freeze
 end
